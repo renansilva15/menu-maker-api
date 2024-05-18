314a4d526bec88f5d70a84917925dd63deb9049b
renansilva15 <renangabrielsilva150@gmail.com>
2024-05-18 19:14:34 -0300

    chore: add database service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+// import { PersonsModule } from './modules/persons/persons.module';
 
 @Module({
   imports: [
@@ -8,6 +10,18 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
       envFilePath: ['.env'],
     }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      entities: [`${__dirname}/**/*.entity.{js,ts}`],
+      migrations: [`${__dirname}/migrations/*.{js,ts}`],
+      migrationsRun: true,
+    }),
+    // PersonsModule,
   ],
   controllers: [AppController],
 })
