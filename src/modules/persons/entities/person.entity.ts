d44013f38d2c196dfbfb9a3e1e8250d5ed68c0c0
renansilva15 <renangabrielsilva150@gmail.com>
2024-05-24 21:03:11 -0300

    chore(persosn): add person entity

--- src/modules/persons/entities/person.entity.ts
+++ src/modules/persons/entities/person.entity.ts
@@ -1,1 +1,29 @@
-export class Person {}
+import { DefaultEntity } from 'src/shared/entities/default.entity';
+import { Column, Entity } from 'typeorm';
+
+@Entity({ name: 'persons' })
+export class PersonEntity extends DefaultEntity<PersonEntity> {
+  @Column({ name: 'cpf' })
+  cpf: string;
+
+  @Column({ name: 'email' })
+  email: string;
+
+  @Column({ name: 'pass' })
+  password: string;
+
+  @Column({ name: 'first_name' })
+  firstName: string;
+
+  @Column({ name: 'last_name' })
+  lastName: string;
+
+  @Column({ name: 'is_email_verified' })
+  isEmailVerified: boolean;
+
+  @Column({ name: 'is_first_login' })
+  isFirstLogin: boolean;
+
+  @Column({ name: 'is_banned' })
+  isBanned: boolean;
+}
