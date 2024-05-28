db774be30eaf3d2ed31decdd22dbd1155a7ff42b
renansilva15 <renangabrielsilva150@gmail.com>
2024-05-28 00:20:50 -0300

    docs(`create-person.dto`): add description and example

--- src/modules/persons/dto/create-person.dto.ts
+++ src/modules/persons/dto/create-person.dto.ts
@@ -8,28 +8,45 @@ import {
 import { IsCPF } from 'src/common/decorators/is-cpf.decorator';
 
 export class CreatePersonDto {
-  @ApiProperty()
+  @ApiProperty({
+    description:
+      'CPF (Cadastro de Pessoas Físicas) is the Brazilian individual taxpayer registry identification',
+    example: '111.444.777-35',
+  })
   @IsString()
   @IsNotEmpty()
   @IsCPF()
   cpf: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: 'Email address of the person',
+    example: 'example@example.com',
+  })
   @IsString()
   @IsEmail()
   email: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    description:
+      'Strong password for the person. Must include uppercase, lowercase, numbers, and special characters',
+    example: 'Password123@',
+  })
   @IsString()
   @IsStrongPassword()
   password: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: 'First name of the person',
+    example: 'John',
+  })
   @IsString()
   @IsNotEmpty()
   firstName: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: 'Last name of the person',
+    example: 'Doe',
+  })
   @IsString()
   @IsNotEmpty()
   lastName: string;
